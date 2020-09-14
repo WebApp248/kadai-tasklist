b28f5618926d44063cd9b371c7e33bbf1987b46f
WebApp248 <twins-tebumpf@memoad.jp>
2020-09-14 11:40:48 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,15 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
+  before_action :correct_user, only: [:destroy]
+
   def index
-    @tasks = Task.all
+    #@tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   def show
-    set_task
+    #set_task
     #@task = Task.find(params[:id])
   end
 
@@ -15,8 +18,9 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(tasklist_params)
-
+    #@task = Task.new(tasklist_params)
+    @task = current_user.tasks.new(tasklist_params)
+    
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
@@ -27,12 +31,12 @@ class TasksController < ApplicationController
   end
 
   def edit
-    set_task
+    #set_task
     #@task = Task.find(params[:id])
   end
 
   def update
-    set_task
+    #set_task
     #@task = Task.find(params[:id])
 
     if @task.update(tasklist_params)
@@ -45,7 +49,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    set_task
+    #set_task
     #@task = Task.find(params[:id])
     @task.destroy
 
@@ -59,8 +63,15 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
-  # Strong Parameter
-    def tasklist_params
-      params.require(:task).permit(:content, :status)
+  def tasklist_params
+    params.require(:task).permit(:content, :status)
+  end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
     end
+  end
+
 end
